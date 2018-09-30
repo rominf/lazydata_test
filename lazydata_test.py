607e6f1f4b55e29db8b0ec081f54b60854904834
Roman Inflianskas <infroma@gmail.com>
2018-09-30 12:07:02 +0300

    Change data location

--- lazydata_test.py
+++ lazydata_test.py
@@ -4,8 +4,6 @@ from lazydata import track
 from hunspell import HunSpell
 
 
-h = HunSpell(track('./profanity_filter/data/en.dic',
-                   'https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.dic'),
-             track('./profanity_filter/data/en.aff',
-                   'https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.aff'))
+h = HunSpell(track('./data/en.dic', 'https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.dic'),
+             track('./data/en.aff', 'https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.aff'))
 print([x.encode(h.get_dic_encoding()) for x in h.suggest('hollo')])
